82b0784dae201c12c51d6d0a80f2b536876a4d94
sakirdemirhan <berxo_umut73@hotmail.com>
2016-03-10 19:28:01 +0200

    Added files via upload

---
+++ send.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import smtplib 
+SUBJECT = 'Rapor'
+TEXT = "Klavyeden yasaklı kelime ya da kelimelerden girildi."
+content = 'Subject: %s\n\n%s' % (SUBJECT, TEXT)
+mail = smtplib.SMTP('smtp.live.com',587) 
+mail.ehlo()
+mail.starttls()
+mail.login('sd@hotmail.com' , '123456' )
+mail.sendmail ('sd@gmail.com' , 'sd@gmail.com' , content)
+mail.quit()
+print("yess")
+
